62f26580c706dc8ca298425e831e402440d484d0
Girly Gado <girlygado.dev@gmail.com>
2026-02-13 18:29:03 +0800

    chore: use next.js proxy route to access tokens on live

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,8 @@ import { ACCESS_TOKEN } from '@/constants/storage-keys';
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  // baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: '/api/proxy',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
@@ -10,18 +11,18 @@ const api = axios.create({
 });
 
 // Add access token automatically if available
-api.interceptors.request.use(
-  (config) => {
-    if (typeof window !== 'undefined') {
-      const token = localStorage.getItem(ACCESS_TOKEN);
-      if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-      }
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+// api.interceptors.request.use(
+//   (config) => {
+//     if (typeof window !== 'undefined') {
+//       const token = localStorage.getItem(ACCESS_TOKEN);
+//       if (token) {
+//         config.headers.Authorization = `Bearer ${token}`;
+//       }
+//     }
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
 
 // Handle invalid/expired token responses
 api.interceptors.response.use(
--- src/services/settingService.ts
+++ src/services/settingService.ts
@@ -1,4 +1,11 @@
-import api from '@/lib/axios';
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
 
 /**
  * Fetch Individual Adapts Setting
